ad062888b98d94e6b7bd89ff8dce13a3e9fc432f
bluepnwage <a.carty2555@gmail.com>
2023-03-15 22:45:40 -0400

    slightly improve homepage design

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 import styles from "./page.module.css";
 import Link from "next/link";
-import { Test } from "./test";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className="display-large mb-2">SXM Quiz</h1>
-      <h2 className="display-medium mb-10 text-center">
+      <h1 className="display-large mb-2 text-on-background">SXM Quiz</h1>
+      <h2 className="display-medium mb-10 text-center text-on-background">
         Test your knowledge of Saint Martin - Are you a local expert or still a tourist?
       </h2>
-      <p className="headline-large">Choose a category</p>
+      <p className="headline-large mb-4 text-on-background">Choose a category</p>
       <div className="flex flex-col gap-4 text-center">
-        <Link href={"/quiz/general"} className="text-secondary">
-          General questions
-        </Link>
-        <Link href={"/quiz/geography"} className="text-secondary">
-          Geography questions
-        </Link>
+        <button
+          className={`px-16 py-4 relative rounded-full text-xl bg-primary 
+        text-on-primary active:top-[2px]`}
+        >
+          Play
+        </button>
       </div>
     </main>
   );
