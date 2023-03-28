73bbf63d6323d14487811f603cd0c448c86c4162
bluepnwage <a.carty2555@gmail.com>
2023-03-28 12:21:50 -0400

    use prisma adapter with planetscale for next-auth

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
+    })
+    // ...add more providers here
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  callbacks: {
+    jwt: async ({ token, account }) => {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = {
+        // @ts-ignore
+        id: token.sub,
+        ...session.user
+      };
+      return session;
+    }
+  }
+};
+
+export default NextAuth(authOptions);
