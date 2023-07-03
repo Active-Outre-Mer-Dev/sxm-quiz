24b135f2ba799068313c91b3bb71b0558c9fc5ba
bluepnwage <a.carty2555@gmail.com>
2023-07-03 08:06:01 -0400

    fix merge conflicts

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,4 +1,4 @@
-import { EmailTemplate } from "./email-template";
+// import { EmailTemplate } from "./email-template";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -6,7 +6,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request: Request) {
   const headers = new Headers();
   headers.set("Content-Type", "application/json");
-  console.log(request.headers.get("Content-Type"));
   const form = await request.formData();
   const message = form.get("message");
   const title = form.get("title");
@@ -18,11 +17,16 @@ export async function POST(request: Request) {
       from: "onboarding@resend.dev",
       to: "a.carty2555@gmail.com",
       subject: `Feedback for ${title}`,
-      react: EmailTemplate({ message: message.toString() })
+      html: `
+      <div>
+        <p>${message}</p>
+      </div>
+      `
     });
 
     return new Response(JSON.stringify({ message: "Email sent!" }), { status: 200, headers });
   } catch (error) {
+    console.log(error);
     return new Response(JSON.stringify({ message: "An error occurred" }), { status: 500, headers });
   }
 }
