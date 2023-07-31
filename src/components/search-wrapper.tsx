8bd8075f01e5bb801b8911193e56bbfef7b9bece
Agis Carty <a.carty2555@gmail.com>
2023-07-31 14:27:28 -0400

    [chore] - add blogs to command menu

--- src/components/search-wrapper.tsx
+++ src/components/search-wrapper.tsx
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/types/database.types";
 import { SearchbarLoading } from "./searchbar-loading";
-import { allArticles } from "contentlayer/generated";
+import { allArticles, allBlogs } from "contentlayer/generated";
 import dynamic from "next/dynamic";
 
-const Searchbar = dynamic(() => import("./searchbar"), { ssr: false, loading:() => <SearchbarLoading/> });
+const Searchbar = dynamic(() => import("./searchbar"), { ssr: false, loading: () => <SearchbarLoading /> });
 
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -16,5 +16,7 @@ export async function SearchWrapper() {
   if (error) throw new Error("Failed to retrieve articles");
   const multipleChoice = data.filter(quiz => quiz.type === "multiple_choice");
   const nameAll = data.filter(quiz => quiz.type === "list");
-  return <Searchbar articles={allArticles} multipleChoice={multipleChoice} nameAll={nameAll} />;
+  return (
+    <Searchbar blogs={allBlogs} articles={allArticles} multipleChoice={multipleChoice} nameAll={nameAll} />
+  );
 }
