739fa655742ef7f80a453e6260d365d6434b5b8f
Agis Carty <a.carty2555@gmail.com>
2023-07-31 10:49:00 -0400

    [fix] - fix mixpanel error in ci

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,11 +1,14 @@
 import Mixpanel from "mixpanel";
 
-const mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN!);
+const mixpanelToken = process.env.MIXPANEL_TOKEN!;
+
+const mixpanel = mixpanelToken ? Mixpanel.init(mixpanelToken) : null;
 
 export const dynamic = "force-dynamic";
 
 export const POST = async (req: Request) => {
   try {
+    if (!mixpanel) return new Response("Nice", { status: 200 });
     const json = await req.json();
     mixpanel.track(json.name, json.properties);
     return new Response("Nice", { status: 200 });
