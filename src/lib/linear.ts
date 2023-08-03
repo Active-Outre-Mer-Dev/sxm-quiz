5a209f0fea353594fa2b47631c0dc0a1e738d7e0
Agis Carty <a.carty2555@gmail.com>
2023-08-03 11:03:15 -0400

    [fix] - apikey error

--- src/lib/linear.ts
+++ src/lib/linear.ts
@@ -1,16 +1,16 @@
 import { LinearClient } from "@linear/sdk";
 
 const apiKey = process.env.LINEAR_PERSONAL_TOKEN!;
-const linear = new LinearClient({ apiKey });
+const linear = apiKey ? new LinearClient({ apiKey }) : null;
 
 const projectId = "87b015c6-bccb-450f-b5d6-b4a42fe94229";
 const teamId = "f60ef876-38d7-4543-8e6e-86ee7715366e";
 
 export async function createIssue(title: string, description: string) {
   try {
+    if (!linear) throw new Error("Must provide api key");
     const labels = await linear.issueLabels();
     const allLabels = labels.nodes.filter(label => label.name === "User created" || label.name === "Bug");
-    console.log(allLabels);
     await linear.createIssue({
       teamId,
       title,
@@ -20,6 +20,10 @@ export async function createIssue(title: string, description: string) {
     });
     return { error: false, message: "Issue created." };
   } catch (error) {
-    return { error: true, message: "Failed to create issue.", stack: error };
+    if (error instanceof Error) {
+      return { error: true, message: error.message, stack: error };
+    } else {
+      return { error: true, message: "An error occurred", stack: error };
+    }
   }
 }
