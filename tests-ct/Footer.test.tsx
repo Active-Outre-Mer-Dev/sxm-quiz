02230aea99125433eff52644b91ab073434741a6
Agis Carty <a.carty2555@gmail.com>
2024-02-18 10:42:01 -0400

    fix tests

--- tests-ct/Footer.test.tsx
+++ tests-ct/Footer.test.tsx
@@ -3,5 +3,6 @@ import { Footer } from "../src/components/site-footer";
 
 test("should contain correct text", async ({ mount }) => {
   const component = await mount(<Footer />);
-  await expect(component).toContainText("Built by AOMDev, 2023");
+  const year = new Date().getFullYear();
+  await expect(component).toContainText(`Built by AOMDev, ${year}`);
 });
