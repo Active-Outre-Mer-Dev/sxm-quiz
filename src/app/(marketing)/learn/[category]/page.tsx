8d315ed2e8d0bdf89cf398dcc92179d006475aff
Agis Carty <91766912+bluepnwage@users.noreply.github.com>
2024-05-09 08:28:24 -0400

    remove static rendering (#49)

--- src/app/(marketing)/learn/[category]/page.tsx
+++ src/app/(marketing)/learn/[category]/page.tsx
@@ -1,22 +1,17 @@
 import { Title } from "@aomdev/ui";
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/types/database.types";
 import { allArticles } from "contentlayer/generated";
 import { Article } from "../_components/article";
+import { createClient } from "@/lib/supabase";
 
 const categories = ["history", "geography", "environment", "economy"];
 
 export function generateStaticParams() {
-  return categories.map(category => ({ category }));
+  return categories.map((category) => ({ category }));
 }
-
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
-);
 
 export default async function Page({ params }: { params: { category: string } }) {
   const contentArticles = allArticles.filter(({ category }) => category === params.category);
+  const supabase = createClient("server_component");
   const { data, error } = await supabase
     .from("articles")
     .select("created_at, slug, featured, community, views")
@@ -24,7 +19,7 @@ export default async function Page({ params }: { params: { category: string } })
   if (error) throw new Error("There was an error fetching the articles");
 
   const articles = contentArticles.map(({ slug, title, thumbnail, intro, category, author, profile }) => {
-    const articleMetadata = data.find(article => article.slug === slug);
+    const articleMetadata = data.find((article) => article.slug === slug);
 
     if (!articleMetadata) throw new Error(`Must add  ${slug} article metadata to supabase`);
     return {
@@ -39,12 +34,20 @@ export default async function Page({ params }: { params: { category: string } })
   });
   return (
     <div className="min-h-screen mb-20">
-      <Title order={1} className="font-heading text-center capitalize mb-10 font-medium">
+      <Title
+        order={1}
+        className="font-heading text-center capitalize mb-10 font-medium"
+      >
         {params.category} Articles
       </Title>
       <section className="w-full b mb-36 grid grid-cols-1 lg:grid-cols-2 gap-x-5 gap-y-10">
-        {articles.map(article => {
-          return <Article {...article} key={article.slug} />;
+        {articles.map((article) => {
+          return (
+            <Article
+              {...article}
+              key={article.slug}
+            />
+          );
         })}
       </section>
     </div>
