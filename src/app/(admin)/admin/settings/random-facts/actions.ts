60bb5fcfcc88f27e523561096514d53b2007b9e6
Agis Carty <a.carty2555@gmail.com>
2024-05-15 00:33:11 -0400

    can upload facts with .txt file

--- src/app/(admin)/admin/settings/random-facts/actions.ts
+++ src/app/(admin)/admin/settings/random-facts/actions.ts
@@ -4,16 +4,33 @@ import { ActionReturn, errorActionReturn, successActionReturn } from "@/lib/acti
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
-const FactSchema = z.object({ description: z.string() });
+const FactSchema = z.object({ description: z.string().optional(), file_facts: z.string().optional() });
 export type FactSchemaType = z.infer<typeof FactSchema>;
 export type FactSchemaState = ActionReturn<FactSchemaType>;
 
 export const addFact = async (prevState: FactSchemaState, formData: FormData): Promise<FactSchemaState> => {
   "use server";
   const schema = FactSchema.safeParse(Object.fromEntries(formData));
-  console.log("bruh");
+
   if (schema.success) {
-    await createClient("server_action").from("random_facts").insert(schema.data);
+    if (!schema.data.description && !schema.data.file_facts) {
+      return errorActionReturn({
+        message: "Must enter facts either through text field or by uploading a .txt file",
+        inputErrors: null
+      });
+    }
+    if (schema.data.description) {
+      await createClient("server_action")
+        .from("random_facts")
+        .insert({ description: schema.data.description });
+    }
+    if (schema.data.file_facts) {
+      const allFacts = schema.data.file_facts
+        .split("@")
+        .filter((fact) => fact)
+        .map((fact) => ({ description: fact }));
+      await createClient("server_action").from("random_facts").insert(allFacts);
+    }
     revalidatePath("/admin/settings/random-facts");
 
     return successActionReturn("Fact added");
--- src/app/(admin)/admin/settings/random-facts/fact-form.tsx
+++ src/app/(admin)/admin/settings/random-facts/fact-form.tsx
@@ -1,40 +1,112 @@
 "use client";
-import { FactSchemaType, addFact } from "./actions";
-import { Button, Textarea } from "@aomdev/ui";
+import { addFact } from "./actions";
+import { Button, Textarea, Tooltip, Alert } from "@aomdev/ui";
 import { useActionState } from "@/lib/hooks/use-action-state";
 import { FileText } from "lucide-react";
+import { useRef, useState } from "react";
+import { cardStyles } from "@aomdev/ui/src/card/styles";
+import type { FormEvent } from "react";
+import type { FactSchemaType } from "./actions";
 
 export function FactForm() {
-  const { ref, formAction } = useActionState<FactSchemaType>(addFact, {
+  const { ref, formAction, state } = useActionState<FactSchemaType>(addFact, {
     resetOnSuccess: true,
     onSuccess(message) {
       alert(message);
     }
   });
+  const [fileFacts, setFileFacts] = useState("");
+  const [fileName, setFileName] = useState("");
+  const fileRef = useRef<HTMLInputElement>(null);
 
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files) {
+      const file = e.currentTarget.files[0];
+      if (!file) return;
+      setFileName(file.name);
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const result = e.target?.result as string;
+        console.log(result.split("\n"));
+        setFileFacts(result.split("\n").join("@"));
+      };
+      fileReader.readAsText(file);
+    }
+  };
+
+  const onClick = () => {
+    fileRef.current?.click();
+  };
+
+  const onClear = () => {
+    setFileFacts("");
+    setFileName("");
+    if (fileRef.current) {
+      fileRef.current.removeAttribute("value");
+    }
+  };
   return (
-    <form
-      ref={ref}
-      action={formAction}
-      className="space-y-4 mb-10"
-    >
-      <Textarea
-        label="New fact"
-        name="description"
-      />
-      <div className="flex gap-4">
-        <Button>Submit</Button>
-        <Button
-          variant={"neutral"}
-          type="button"
-        >
-          <FileText
-            size={16}
-            className="inline-block mr-2"
-          />
-          Import from csv
-        </Button>
-      </div>
-    </form>
+    <>
+      {state.status === "error" && <Alert color="error">{state.message}</Alert>}
+      <form
+        ref={ref}
+        action={formAction}
+        className="space-y-4 mb-10"
+      >
+        <Textarea
+          label="New fact"
+          name="description"
+        />
+        <input
+          type="text"
+          name="file_facts"
+          hidden
+          defaultValue={fileFacts}
+        />
+        <input
+          ref={fileRef}
+          type="file"
+          onChange={handleChange}
+          hidden
+          accept=".txt"
+        />
+        <div className="flex gap-6 items-center">
+          <Button>Submit</Button>
+          <div className="flex gap-2">
+            <Tooltip delayDuration={0}>
+              <Tooltip.Trigger asChild>
+                <Button
+                  variant={"neutral"}
+                  type="button"
+                  onClick={onClick}
+                >
+                  <FileText
+                    size={16}
+                    className="inline-block mr-2"
+                  />
+                  {fileName ? fileName : "Import from text file"}
+                </Button>
+              </Tooltip.Trigger>
+              <Tooltip.Content
+                sideOffset={10}
+                className={cardStyles()}
+              >
+                Every fact needs to be on a new line
+              </Tooltip.Content>
+            </Tooltip>
+            {fileName && (
+              <Button
+                type="button"
+                onClick={onClear}
+                variant={"outline"}
+                className="ring-0"
+              >
+                Clear
+              </Button>
+            )}
+          </div>
+        </div>
+      </form>
+    </>
   );
 }
