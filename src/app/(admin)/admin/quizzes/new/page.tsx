73f43feb1cf64809d964ca6ce3b26164bff756db
Agis Carty <a.carty2555@gmail.com>
2024-05-19 23:43:22 -0400

    fix quiz bug

--- src/app/(admin)/admin/quizzes/new/page.tsx
+++ src/app/(admin)/admin/quizzes/new/page.tsx
@@ -1,49 +1,13 @@
-import { Textarea, Title, Button } from "@aomdev/ui";
-import { Select } from "./client";
-import { Radio } from "./quiz-type";
-import { QuizTitle } from "./quiz-title";
+import { Title } from "@aomdev/ui";
 import { createClient } from "@/lib/supabase";
-import { z } from "zod";
-import { redirect } from "next/navigation";
+import { CreateQuizForm } from "./form";
 
-const FormSchema = z.object({
-  quiz_title: z.string().min(3),
-  quiz_slug: z.string().min(3),
-  quiz_description: z.string().min(10),
-  quiz_category: z.enum(["history", "geography", "economy", "environment"]),
-  quiz_type: z.enum(["multiple_choice", "list"])
-});
+export default async function Page() {
+  const { data, error } = await createClient("server_component").from("categories").select("*");
+  if (error) throw error;
 
-export default function Page() {
-  const createQuiz = async (formData: FormData) => {
-    "use server";
-    const supabase = createClient("server_action", true);
-    const form = Object.fromEntries(formData);
+  const selectItems = data.map((cat) => ({ label: cat.title, value: cat.id }));
 
-    const status = FormSchema.safeParse(form);
-    console.log(status.success);
-
-    if (!status.success) {
-      console.log(status.error.errors);
-      return status.error.errors;
-    }
-    const { data } = status;
-    const { error, data: quizData } = await supabase
-      .from("quiz")
-      .insert({
-        title: data.quiz_title,
-        category: data.quiz_category,
-        description: data.quiz_description,
-        slug: data.quiz_slug,
-        type: data.quiz_type
-      })
-      .select()
-      .single();
-    console.log(error, quizData, ";wtf");
-    console.log(error, quizData);
-    if (error || !quizData) return;
-    redirect(`/admin/quizzes/${quizData.id}`);
-  };
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -56,34 +20,7 @@ export default function Page() {
           </Title>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            className="space-y-6"
-            action={createQuiz}
-          >
-            <QuizTitle />
-            <Textarea
-              label="Description"
-              name="quiz_description"
-            />
-            <Select
-              name="quiz_category"
-              fullWidth
-              items={[
-                { label: "History", value: "history" },
-                { label: "Geography", value: "geography" },
-                { label: "Economy", value: "economy" },
-                { label: "Environment", value: "environment" }
-              ]}
-            />
-            <Radio />
-
-            <Button
-              type="submit"
-              fullWidth
-            >
-              Sign in
-            </Button>
-          </form>
+          <CreateQuizForm selectItems={selectItems} />
         </div>
       </div>
     </>
--- src/app/(admin)/admin/quizzes/new/quiz-type.tsx
+++ src/app/(admin)/admin/quizzes/new/quiz-type.tsx
@@ -1,16 +1,35 @@
 "use client";
-import { Radio as _Radio } from "@aomdev/ui";
+import { TextInput, Radio as _Radio } from "@aomdev/ui";
+import { useState } from "react";
 
 export function Radio() {
+  const [showTask, setShowTask] = useState("");
   return (
-    <fieldset>
-      <legend className="text-sm select-none font-medium text-gray-800 dark:text-gray-100 block mb-1">
-        Quiz type
-      </legend>
-      <_Radio name="quiz_type">
-        <_Radio.Item label="Multiple choice" value="multiple_choice" />
-        <_Radio.Item label="List Quiz" value="list" />
-      </_Radio>
-    </fieldset>
+    <>
+      <fieldset>
+        <legend className="text-sm select-none font-medium text-gray-800 dark:text-gray-100 block mb-1">
+          Quiz type
+        </legend>
+        <_Radio
+          onValueChange={setShowTask}
+          name="quiz_type"
+        >
+          <_Radio.Item
+            label="Multiple choice"
+            value="multiple_choice"
+          />
+          <_Radio.Item
+            label="List Quiz"
+            value="list"
+          />
+        </_Radio>
+      </fieldset>
+      {showTask === "list" && (
+        <TextInput
+          name="quiz_task"
+          label="Task"
+        />
+      )}
+    </>
   );
 }
